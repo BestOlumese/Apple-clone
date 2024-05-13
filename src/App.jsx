7cf5b5d5bb74f878b11168a44ef0f87e0f86c3e0
BestOlumese <bestolumese@gmail.com>
2024-05-13 14:16:58 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Highlights from "./components/Highlights"
+import Hero from "./components/Hero"
+import Navbar from "./components/Navbar"
+import Model from "./components/Model"
+
+const App = () => {
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Model />
+    </main>
+  )
+}
+
+export default App
